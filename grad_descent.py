0fc99aca59e32ee59bc8a7c03cf5c8feaa3b8f4e
semyonbok <semyon.bok@gmail.com>
2023-02-04 13:46:29 +0000

    Enabled multiple linear regression

--- grad_descent.py
+++ grad_descent.py
@@ -1,7 +1,8 @@
+from numbers import Number
 from typing import Literal
+import numpy as np
 
 # import matplotlib.pyplot as plt
-# import numpy as np
 # from matplotlib.animation import FuncAnimation
 
 """
@@ -13,16 +14,24 @@ Ideas
 
 
 def update_coef(X, y, w, b, alpha, loss_func: Literal["mse", "rmse"] = "mse"):
-    """Assumes X and y are array-likes of same length; w, b, alpha are floats.
-    Updates coefficients w and b of a linear model with gradient descent."""
+    """Assumes `X` and `y` are array-like of same length; `w` is float when `X`
+    is 1-D array and 1-D array of same length as `X`'s width if `X` is 2-D
+    array; `b`, `alpha` are floats; `loss_func` is string representing a loss function.
+    Updates coefficients `w` and `b` of a linear model with gradient descent."""
 
     # find derivatives
     if loss_func == "mse":
         factor = 1
     elif loss_func == "rmse":
         factor = .5 * mse(X, y, w, b)**-.5
-    dl_dw = factor * (-2 * X * (y - (w * X + b))).mean()
-    dl_db = factor * (-2 * (y - (w * X + b))).mean()
+    
+    if isinstance(w, Number):
+        dl_dw = factor * (2 * X * ((w * X + b) - y)).mean()
+        dl_db = factor * (2 * ((w * X + b) - y)).mean()
+    else:
+        error = np.dot(X, w) + b - y  # abs error per each data entry
+        dl_dw = factor * (X * error.reshape((-1,1))).mean(axis=0)
+        dl_db = factor * error.mean()
 
     # update coef
     w -= alpha * dl_dw
@@ -31,9 +40,13 @@ def update_coef(X, y, w, b, alpha, loss_func: Literal["mse", "rmse"] = "mse"):
     return w, b
 
 
-def train(X, y, w, b, alpha, epochs, report=100, loss_func: Literal["mse", "rmse"] = "mse"):
-    """Assumes X and y are array-like of same length; w, b, alpha are float;
-    epochs, report are int. Updates linear regression model epochs times"""
+def train(X, y, w, b, alpha, epochs,
+    report=100, loss_func: Literal["mse", "rmse"] = "mse"):
+    """Assumes `X` and `y` are array-like of same length; `w` is float when `X`
+    is 1-D array and 1-D array of same length as `X`'s width if `X` is 2-D
+    array; `b`, `alpha` are floats; `epochs`, `report` are ints; `loss_func` is
+    string representing a loss function.
+    Updates linear regression model `epochs` times."""
 
     for e in range(epochs):
         w, b = update_coef(X, y, w, b, alpha, loss_func=loss_func)
@@ -42,24 +55,21 @@ def train(X, y, w, b, alpha, epochs, report=100, loss_func: Literal["mse", "rmse
         if e % report == 0:
             loss = eval(loss_func)(X, y, w, b)
             print(
-                f"epoch: {e:.4f}, w: {w:.4f}, b: {b:.4f}, {loss_func}: {loss:.4}")
+                f"epoch: {e}, w: {w}, b: {b:.4f}, {loss_func}: {loss:.4f}")
 
     return w, b
 
 
-def mse(X, y, w, b):
-    """Assumes X and y are array-like of same length; w, b, alpha are float.
+def mse(X, y, w, b) -> float:
+    """Assumes `X` and `y` are array-like of same length; `w` is float when `X`
+    is 1-D array and 1-D array of same length as `X`'s width if `X` is 2-D
+    array; b is float.
     Computes mean squared error."""
+    return (((np.dot(X, w) + b) - y)**2).mean()
 
-    loss = ((y - (w * X + b))**2).mean()
-
-    return loss
-
-
-def rmse(X, y, w, b):
-    """Assumes X and y are array-like of same length; w, b, alpha are float.
+def rmse(X, y, w, b) -> float:
+    """Assumes `X` and `y` are array-like of same length; `w` is float when `X`
+    is 1-D array and 1-D array of same length as `X`'s width if `X` is 2-D
+    array; b is float.
     Computes root mean squared error."""
-
-    loss = ((y - (w * X + b))**2).mean()**.5
-
-    return loss
+    return (((np.dot(X, w) + b) - y)**2).mean()**.5
